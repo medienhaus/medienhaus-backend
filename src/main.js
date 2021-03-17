50a5a34600bc2b242450f5f8e628d2b8f192a323
Florian Weber <florian@weber.world>
2021-03-17 16:30:11 +0100

    Enable CORS only on local environments

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core'
+import { Logger } from '@nestjs/common'
 import { AppModule } from './app.module'
 
 async function bootstrap () {
   const app = await NestFactory.create(AppModule)
+  if (process.env.NODE_ENV === 'local') {
+    Logger.debug('Attention: Enabling CORS')
+    app.enableCors()
+  }
   await app.listen(3001)
 }
 bootstrap()
