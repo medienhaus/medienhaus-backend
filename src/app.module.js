1f46b5ea11159852b30376fcf724bfec324eb269
Florian Weber <florian@weber.world>
2021-03-24 19:16:34 +0100

    Add new middleware to check if request comes with a valid Matrix access token

--- src/app.module.js
+++ src/app.module.js
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { MessengerService } from './messenger.service'
+import { ValidMatrixAccessTokenMiddleware } from './validMatrixAccessToken.middleware'
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [MessengerService]
 })
-export class AppModule {}
+export class AppModule {
+  configure (consumer) {
+    consumer
+      .apply(ValidMatrixAccessTokenMiddleware)
+      .forRoutes(AppController)
+  }
+}
