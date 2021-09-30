61c82690b5197c9837b574639967ab1736a48005
Florian Weber <florian@weber.world>
2021-09-30 15:04:21 +0200

    Add new /rundgang/knock (POST) endpoint

--- src/rundgang.controller.js
+++ src/rundgang.controller.js
@@ -1,5 +1,6 @@
 import { Controller, Dependencies, Post, Get, Bind, Headers, Body } from '@nestjs/common'
 import { MatrixService } from './matrix.service'
+import { filter, size } from 'lodash'
 
 @Controller('rundgang')
 @Dependencies(MatrixService)
@@ -68,5 +69,40 @@ export class RundgangController {
       body: `I hereby accept the terms and conditions (${this.currentTermsAndConditionsHash})`,
       msgtype: 'm.text'
     })
+  }
+
+  // Make the requesting user knock (and possibly auto-join) on the given context space
+  @Post('/knock')
+  @Bind(Headers('medienhaus-matrix-user-id'), Headers('medienhaus-matrix-access-token'), Body('contextSpaceId'))
+  async acceptTerms (matrixUserId, matrixUserAccessToken, contextSpaceId) {
+    const matrixClientBot = this.appService.createMatrixClient(
+      process.env.RUNDGANG_BOT_USERID,
+      process.env.RUNDGANG_BOT_ACCESSTOKEN,
+      process.env.MATRIX_BASE_URL_CONTENT
+    )
+    const matrixClientUser = this.appService.createMatrixClient(
+      matrixUserId,
+      matrixUserAccessToken,
+      process.env.MATRIX_BASE_URL_CONTENT
+    )
+
+    // User knocks on space
+    await matrixClientUser.http.authedRequest(undefined, 'POST', `/knock/${contextSpaceId}`, {}, { prefix: '/_matrix/client/unstable' })
+
+    // Check if we have more than 1 user above power level 50 (rundgang-bot will be the first, any user beyond that will be an actual "user moderator")
+    const contextHasModerators = size(filter((await matrixClientBot.getStateEvent(contextSpaceId, 'm.room.power_levels')).users, (powerLevel, username) => powerLevel >= 50)) > 1
+
+    if (contextHasModerators) {
+      return { joined: false }
+    }
+
+    // If we do not have moderators for the given context space yet, we want the requesting user to be accepted automatically
+    await matrixClientBot.invite(contextSpaceId, matrixClientUser.getUserId()).catch(e => {
+      // Ignore the error if it's just telling us that the user is a member of this room already
+      if (!e.message.includes(`${matrixClientUser.getUserId()} is already in the room`)) { throw e }
+    })
+    await matrixClientUser.joinRoom(contextSpaceId)
+
+    return { joined: true }
   }
 }
